1cb28f8ede262ddf428feb300514228481071261
Heidsieck <gaetan.heidsieck@gmail.com>
2019-12-12 16:56:28 +0100

    Delete start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-
-python src/openalea/distributed/execution/start_evaluation.py -n 1 --nb_plants=1 -a "force_rerun" -c "local" -id 2 -wf "A" --profile="distributed1" --cluster-id=""  --image=True  --mem="one_map"
-
-
