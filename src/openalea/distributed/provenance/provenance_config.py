4241c3da958cf9014305142c2f36ededbbe00317
gaetan heidsieck <gaetan.heidsieck@inria.fr>
2020-01-20 19:35:16 +0100

    Add cassandra provenance

--- src/openalea/distributed/provenance/provenance_config.py
+++ src/openalea/distributed/provenance/provenance_config.py
@@ -4,11 +4,11 @@ from openalea.core import settings
 import os
 from os.path import expanduser
 
+REMOTE_PROV = False
 
+# Files
 PROVENANCE_PATH = path(settings.get_openalea_home_dir()) / 'provenance'
 
-# infos about provenance db
-REMOTE_PROV = False
 # Mongo
 MONGO_ADDR='127.0.0.1'
 MONGO_PORT = 27017
@@ -18,3 +18,9 @@ MONGO_SSH_IP='127.0.0.1'
 home = expanduser("~")
 SSH_PKEY = os.path.join(home, ".ssh", "id_rsa")
 SSH_USERNAME="ubuntu"
+
+
+# cassandra
+CASSANDRA_SSH_IP = "134.158.247.62"
+CASSANDRA_ADDR = "localhost" 
+CASSANDRA_PORT = 9042
