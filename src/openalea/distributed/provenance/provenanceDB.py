c596831e60c861bd5a9c1d115dab562359d1fe18
gaetan heidsieck <gaetan.heidsieck@inria.fr>
2020-01-20 16:50:06 +0100

    Change provenance  so it init from configfile

--- src/openalea/distributed/provenance/provenanceDB.py
+++ src/openalea/distributed/provenance/provenanceDB.py
@@ -1,26 +1,59 @@
+# -*- coding: utf-8 -*-
 from sshtunnel import SSHTunnelForwarder
 import pymongo
 from pymongo.errors import ConnectionFailure
 from sshtunnel import BaseSSHTunnelForwarderError
 import json
-
+import imp
+import os
 from openalea.core.path import path
-
+from openalea.core import settings
 
 
 class ProvFile():
     def __init__(self, *args, **kwargs):
         self.localpath = ""
 
-    def add_prov_item(self, item, *args, **kwargs):
-        print "START WRITING"
-        prov_path = path(self.localpath) / item["workflow"] + '.json'
-        print self.localpath
-        with open(prov_path, 'a+') as fp:
-            json.dump(item, fp, indent=4)
+    def add_task_item(self, item, *args, **kwargs):
+        try:
+            prov_path = os.path.join(self.localpath, str(item["task_id"]) + '.json')
+            with open(prov_path, 'a+') as fp:
+                json.dump(item, fp, indent=4)
+        except:
+            prov_path = path(settings.get_openalea_home_dir()) / 'provenance' / 'task_provenance.json'
+            print('Fail to open prov files. - Now write prov in : ', prov_path)
+            with open(prov_path, 'a+') as fp:
+                json.dump(item, fp, indent=4)
+
+    def add_wf_item(self, item, *args, **kwargs):
+        try:
+            prov_path = os.path.join(self.localpath, str(item["workflow"]) + '.json')
+            with open(prov_path, 'a+') as fp:
+                json.dump(item, fp, indent=4)
+        except:
+            prov_path = path(settings.get_openalea_home_dir()) / 'provenance' / 'wf_provenance.json'
+            print('Fail to open prov files. - Now write prov in : ', prov_path)
+            with open(prov_path, 'a+') as fp:
+                json.dump(item, fp, indent=4)
+    
+    def is_in(self, task_id=None, wf_id=None):
+        pass
+
+    def show(self):
+        pass
+
+    def remove_all_item(self):
+        pass
 
     def init(self, *args, **kwargs):
-        self.localpath = kwargs["path"]
+        path_config =  kwargs.get('provenance_config', None)
+        if path_config == None:
+            import openalea.distributed.provenance.provenance_config as prov_config
+        else:
+            prov_config = imp.load_source('prov_config', path_config)
+            import prov_config
+        print("Load provenance config from : ", prov_config.__file__)
+        self.localpath = prov_config.PROVENANCE_PATH
 
     def close(self):
         pass
@@ -57,17 +90,9 @@ class ProvMongo():
         # add element to index db
         self.taskdb.insert_one(item)
 
-    def add_list_task_item(self, itemlist, *args, **kwargs):
-        for item in itemlist:
-            self.taskdb.insert_one(item)
-
     def add_wf_item(self, item, *args, **kwargs):
         # add element to index db
         self.wfdb.insert_one(item)
-
-    def add_list_wf_item(self, itemlist, *args, **kwargs):
-        for item in itemlist:
-            self.wfdb.insert_one(item)
 
     def show(self, task_id=None, wf_id=None):
         print("the task provenance has : ", self.taskdb.count(), " entries :")
@@ -77,10 +102,10 @@ class ProvMongo():
     def start_sshtunnel(self, *args, **kwargs):
         try:
             self.server = SSHTunnelForwarder(
-                ssh_address_or_host=kwargs['ssh_ip_addr'],
-                ssh_pkey=kwargs['ssh_pkey'],
-                ssh_username=kwargs['ssh_username'],
-                remote_bind_address=kwargs['remote_bind_address']
+                ssh_address_or_host=kwargs.get('ssh_ip_addr'),
+                ssh_pkey=kwargs.get('ssh_pkey'),
+                ssh_username=kwargs.get('ssh_username'),
+                remote_bind_address=kwargs.get('remote_bind_address')
                 # ,
                 # *args,
                 # **kwargs
@@ -96,7 +121,7 @@ class ProvMongo():
                 print "SSH Server not started - cannot connect to Mongo"
                 return
             try:
-                client = pymongo.MongoClient(host=kwargs['mongo_ip_addr'],
+                client = pymongo.MongoClient(host=kwargs.get('mongo_ip_addr'),
                                              port=self.server.local_bind_port
                                              # ,
                                              # , # server.local_bind_port is assigned local port
@@ -114,8 +139,8 @@ class ProvMongo():
                 print "failed to connect to mongodb"
         else:
             try:
-                client = pymongo.MongoClient(host=kwargs['mongo_ip_addr'],
-                                             port=kwargs['mongo_port']
+                client = pymongo.MongoClient(host=kwargs.get('mongo_ip_addr'),
+                                             port=kwargs.get('mongo_port')
                                              # ,
                                              # , # server.local_bind_port is assigned local port
                                              # username='admin',
@@ -143,13 +168,29 @@ class ProvMongo():
         return 0
 
     def init(self, *args, **kwargs):
-        if kwargs['remote']:
-            self.remote=True
-            self.start_sshtunnel(*args, **kwargs)
-            self.start_client(*args, **kwargs)
+        path_config =  kwargs.get('provenance_config', None)
+        if path_config == None:
+            import openalea.distributed.provenance.provenance_config as prov_config
         else:
+            prov_config = imp.load_source('prov_config', path_config)
+            import prov_config
+        print("Load provenance config from : ", prov_config.__file__)
+        # self.localpath = prov_config.PROVENANCE_PATH
+        if prov_config.REMOTE_PROV:
+            self.remote=True
+            self.start_sshtunnel(ssh_ip_addr=prov_config.MONGO_SSH_IP,
+                                ssh_pkey=prov_config.SSH_PKEY,
+                                ssh_username=prov_config.SSH_USERNAME,
+                                remote_bind_address=(prov_config.MONGO_ADDR, prov_config.MONGO_PORT) , 
+                                *args, **kwargs)
+        else: 
             self.remote=False
-            self.start_client(*args, **kwargs)
+        self.start_client(mongo_ip_addr=prov_config.MONGO_ADDR,
+                          mongo_port=prov_config.MONGO_PORT,
+                          *args, **kwargs)
+
+
+
 
     def close(self):
         if self.remote:
@@ -157,3 +198,19 @@ class ProvMongo():
             self.close_sshtunel()
         else:
             self.close_client()
+
+
+def start_provdb(provenance_config=None, provenance_type="Files"):
+    if provenance_type == "Files":
+        provdb = ProvFile()
+        provdb.init(provenance_config)
+        return provdb
+    if provenance_type == "MongoDB":
+        provdb = ProvMongo()
+        provdb.init(provenance_config)
+        return provdb
+    if provenance_type == "Cassandra":
+        # provdb = ProvMongo()
+        # provdb.init(provenance_config)
+        # return provdb
+        pass
--- src/openalea/distributed/provenance/provenance_config.py
+++ src/openalea/distributed/provenance/provenance_config.py
@@ -4,11 +4,8 @@ from openalea.core import settings
 import os
 from os.path import expanduser
 
-# PATHS
-# files infos
+
 PROVENANCE_PATH = path(settings.get_openalea_home_dir()) / 'provenance'
-TMP_PATH = path(settings.get_openalea_home_dir()) / "execution_data"
-CACHE_PATH = path(settings.get_openalea_home_dir()) / "cached_data"
 
 # infos about provenance db
 REMOTE_PROV = False
@@ -16,16 +13,8 @@ REMOTE_PROV = False
 MONGO_ADDR='127.0.0.1'
 MONGO_PORT = 27017
 
-# cassandra
-REMOTE_INDEX = True
-CASSANDRA_SSH_IP = "134.158.247.62"
-CASSANDRA_PORT = 9042
-
-
-# SSH
-PROVDB_SSH_ADDR = ""
+# remote db
+MONGO_SSH_IP='127.0.0.1'
 home = expanduser("~")
 SSH_PKEY = os.path.join(home, ".ssh", "id_rsa")
 SSH_USERNAME="ubuntu"
-# REMOTE_DB_ADDR=('127.0.0.1', 27017)
-
