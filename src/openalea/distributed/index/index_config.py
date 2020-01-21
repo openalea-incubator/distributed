408d4acdb8a46fe4fae2b4da9da2debe857d54e5
gaetan heidsieck <gaetan.heidsieck@inria.fr>
2020-01-21 18:11:48 +0100

    use a config file for index

---
+++ src/openalea/distributed/index/index_config.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from openalea.core.path import path
+from openalea.core import settings
+import os
+from os.path import expanduser
+
+REMOTE_INDEX = True
+
+# Files
+INDEX_PATH = path(settings.get_openalea_home_dir()) / 'index'
+
+# Mongo
+MONGO_ADDR='127.0.0.1'
+MONGO_PORT = 27017
+
+# remote db
+MONGO_SSH_IP='127.0.0.1'
+home = expanduser("~")
+SSH_PKEY = os.path.join(home, ".ssh", "id_rsa")
+SSH_USERNAME="ubuntu"
+
+
+# cassandra
+CASSANDRA_SSH_IP = "134.158.247.62"
+CASSANDRA_ADDR = "localhost" 
+CASSANDRA_PORT = 9042
